88ff3da78405d3f599fb9fbc13ab7d64048d445c
Ruiso-Malue <ngaboy12345@gmail.com>
2022-08-26 14:39:36 +0530

    First commit

---
+++ MVCORE3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCORE3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
